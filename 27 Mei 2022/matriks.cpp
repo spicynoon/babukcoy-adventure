3fac76cff44289de420b3d469321363bdb41f2c1
Yandi Yanun <93895831+yan00n@users.noreply.github.com>
2022-05-31 09:39:45 +0700

    Update matriks.cpp

--- 27 Mei 2022/matriks.cpp
+++ 27 Mei 2022/matriks.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <conio.h>
+#include <time.h>
+#include <stdlib.h> 
 using namespace std;
 
-#define KEY_UP 72
-#define KEY_DOWN 80
-#define KEY_LEFT 75
-#define KEY_RIGHT 77
+#define KEY_UP 119
+#define KEY_DOWN 115
+#define KEY_LEFT 97
+#define KEY_RIGHT 100
 
 struct character_t
 {
@@ -14,16 +16,24 @@ struct character_t
     int hp;
 };
 
+struct item_t
+{
+    string name;
+    string type;
+    int armorBuffeffect;
+    int attackBuffeffect;
+};
+
 
 int main(){
     /*
     RULES
     1. lane (able)
-    2. ambush (able/hide)
-    3. puddle (able)
+    2. ambush (able || random find enemy 15%)
+    3. puddle (able || random find item 10%)
     4. tree (disable)
     5. stone (disable)
-    6. waters (able || random 0,0 - 1,0 || trap = < 0,2)
+    6. waters (able || random find item 10%)
     RULES TILE
     1. true jika ada di 1, 2, 3, 6
     DESAIN DATA
@@ -59,13 +69,47 @@ int main(){
         },
     };
 
+    item_t item[] = {
+        {
+            "Tameng",
+            "Armor",
+            200,
+            5
+        }, 
+        {
+            "Kujang",
+            "Attack",
+            5,
+            125
+        },
+        {
+            "Zirah",
+            "Armor",
+            500,
+            10
+        },
+        {
+            "Keling",
+            "Attack",
+            0,
+            75,
+        },
+        {
+            "Trisula",
+            "Attack",
+            10,
+            250
+        },
+    };
+
     int panjangMap = 10;    //x
     int lebarMap = 10;      //y
     
     int koorCharY = 0;
     int koorCharX = 0;
     
-    cout << "karakter berada di (" << koorCharY << ", " << koorCharX << ")" << endl;
+    cout << "u spawn at (" << koorCharY << ", " << koorCharX << ")" << endl
+    << "use w,a,s,d to move" << endl;
     
     int map[lebarMap][panjangMap] = {
         {1,1,1,1,1,2,2,1,6,4},
@@ -85,28 +129,26 @@ int main(){
     while (1){
         
         // Input arrow key
-        cout << "Masukkan arrow key :";
-        cin >> arrowKey;
+        cout << "input arrow key :";
+        arrowKey = (getch()); 
+        cout << endl;
         
         // Bergerak ke atas
-        if (arrowKey == 72 && (map[koorCharY-1][koorCharX] == 1 || map[koorCharY-1][koorCharX] == 4) && koorCharY >= 0){
+        if (arrowKey == 119 && (map[koorCharY-1][koorCharX] == 1 || map[koorCharY-1][koorCharX] == 4) && koorCharY >= 0){
             koorCharY = koorCharY-1;
         }
         // Bergerak ke bawah
-        if (arrowKey == 80 && (map[koorCharY+1][koorCharX] == 1 || map[koorCharY+1][koorCharX] == 4) && koorCharY < lebarMap){
+        if (arrowKey == 115 && (map[koorCharY+1][koorCharX] == 1 || map[koorCharY+1][koorCharX] == 4) && koorCharY < lebarMap){
             koorCharY = koorCharY+1;
         }
         // Bergerak ke kiri
-        if (arrowKey == 75 && (map[koorCharY][koorCharX-1] == 1 || map[koorCharY][koorCharX-1] == 4) && koorCharY >= 0){
+        if (arrowKey == 97 && (map[koorCharY][koorCharX-1] == 1 || map[koorCharY][koorCharX-1] == 4) && koorCharY >= 0){
             koorCharX = koorCharX-1;
         }
         // Bergerak ke kanan
-        if (arrowKey == 77 && (map[koorCharY][koorCharX+1] == 1 || map[koorCharY][koorCharX+1] == 4) && koorCharY < panjangMap){
+        if (arrowKey == 100 && (map[koorCharY][koorCharX+1] == 1 || map[koorCharY][koorCharX+1] == 4) && koorCharY < panjangMap){
             koorCharX = koorCharX+1;
         }
-
-        // Random
-
         // Render
         for(int y=0; y<lebarMap; y++){
             for(int x=0; x<panjangMap; x++){
@@ -118,6 +160,32 @@ int main(){
                 }
             }
             cout << endl;
+        }
+        //Random trap and enemy
+        int characterPosition=map[koorCharY][koorCharX];
+        if (characterPosition==2)
+        {
+            int probabilityFindEnemy;
+            srand (time(NULL));
+            probabilityFindEnemy = rand() %15;
+
+            if (probabilityFindEnemy<3)
+            {
+                cout << "u find the enemy" << endl
+                << "ready to fight bruh !!" << endl;
+            }
+        }
+        if (characterPosition==3 || characterPosition==6)
+        {
+            int probabilityFoundItem;
+            srand (time(NULL));
+            probabilityFoundItem = rand() %10;
+
+            if (probabilityFoundItem<3)
+            {
+                cout << "u found item" << endl
+                << "item will automatically saved" << endl;
+            }
         }
     }
     return 0;
